a10857b0fc7644f30d3753f8b0a8330bbf8375a2
Harry Hartley <hhartley94@gmail.com>
2023-12-15 10:27:21 +0000

    add episodes, albums, artists, genres, mixes and rels to schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,27 +1,20 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import {
-  bigint,
+  date,
   index,
+  int,
   mysqlTableCreator,
+  serial,
   timestamp,
   varchar,
 } from "drizzle-orm/mysql-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const mysqlTable = mysqlTableCreator((name) => `modulation_${name}`);
 
 export const posts = mysqlTable(
   "post",
   {
-    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
+    id: serial("id").primaryKey(),
     name: varchar("name", { length: 256 }),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
@@ -30,5 +23,124 @@ export const posts = mysqlTable(
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
+);
+
+export const episodes = mysqlTable(
+  "episode",
+  {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 256 }),
+    number: int("number"),
+    imageUrl: varchar("image_url", { length: 256 }),
+    spotifyUrl: varchar("spotify_url", { length: 256 }),
+    mixId: int("mix_id"),
+    releasedAt: date("released_at"),
+    createdAt: timestamp("created_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updatedAt").onUpdateNow(),
+  },
+  (episode) => ({
+    nameIndex: index("name_idx").on(episode.name),
+  }),
+);
+
+export const episodesRelations = relations(episodes, ({ one }) => ({
+  mix: one(mixes, {
+    fields: [episodes.mixId],
+    references: [mixes.id],
+  }),
+}));
+
+export const mixes = mysqlTable("mix", {
+  id: serial("id").primaryKey(),
+  spotifyUrl: varchar("spotify_url", { length: 256 }),
+  createdAt: timestamp("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updatedAt").onUpdateNow(),
+});
+
+export const albums = mysqlTable(
+  "album",
+  {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 256 }),
+    artistId: int("artist_id"),
+    genreId: int("artist_id"),
+    releasedAt: date("released_at"),
+    createdAt: timestamp("created_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updatedAt").onUpdateNow(),
+  },
+  (album) => ({
+    nameIndex: index("name_idx").on(album.name),
+  }),
+);
+
+export const albumsRelations = relations(albums, ({ one }) => ({
+  artist: one(artists, {
+    fields: [albums.artistId],
+    references: [artists.id],
+  }),
+  genre: one(genres, {
+    fields: [albums.genreId],
+    references: [genres.id],
+  }),
+}));
+
+export const artists = mysqlTable(
+  "artist",
+  {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 256 }),
+    createdAt: timestamp("created_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updatedAt").onUpdateNow(),
+  },
+  (artist) => ({
+    nameIndex: index("name_idx").on(artist.name),
+  }),
+);
+
+export const genres = mysqlTable(
+  "genre",
+  {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 256 }),
+    createdAt: timestamp("created_at")
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updatedAt").onUpdateNow(),
+  },
+  (genre) => ({
+    nameIndex: index("name_idx").on(genre.name),
+  }),
+);
+
+export const relAlbumsEpisodes = mysqlTable("rel_albums_episodes", {
+  id: serial("id").primaryKey(),
+  albumId: int("album_id"),
+  episodeId: int("episode_id"),
+  createdAt: timestamp("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updatedAt").onUpdateNow(),
+});
+
+export const relAlbumsEpisodesRelations = relations(
+  relAlbumsEpisodes,
+  ({ one }) => ({
+    artist: one(albums, {
+      fields: [relAlbumsEpisodes.albumId],
+      references: [albums.id],
+    }),
+    genre: one(episodes, {
+      fields: [relAlbumsEpisodes.episodeId],
+      references: [episodes.id],
+    }),
+  }),
 );
